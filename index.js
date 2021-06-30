80a29ecb6d56c55811e96f31df2bf93ceca2cebc
choidoorim <doorim321@naver.com>
2021-07-01 01:56:13 +0900

    포트 나누기

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('./config/express');
 const {logger} = require('./config/winston');
+let port;
 
-const port = 3000;
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "development"){
+    port = 3000;
+}else {
+    port = 9000;
+}
+
 express().listen(port);
 logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
